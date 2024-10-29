8d25c0d45a429a17ae72c9bfbcc884426078e9f4
Payal Lakra <payallakra307@gmail.com>
2024-10-29 21:51:47 +0530

    Day7

--- Day7.py
+++ Day7.py
@@ -2,3 +2,36 @@
 Topics to be covered:
 - Hangman Game
 '''
+
+import random
+
+words = ['payal','nikki','shaksham','vinni','tannu']
+
+choose = random.choice(words)
+print(choose)
+
+word_length = len(choose)
+placeholder = ""
+for i in range(word_length):
+    placeholder += "_ "
+print(placeholder)
+
+game_over = False
+correct_letter = []
+display = ""
+while not game_over:
+    guess = input("Enter a letter:").lower()
+
+    for letter in choose:
+        if letter == guess:
+            display += letter
+            correct_letter.append(guess)
+        elif letter in correct_letter:
+            display += letter
+        else:
+            display += "_ "
+    print(display)
+
+    if "_" not in guess:
+        game_over = True
+        print("You Win")

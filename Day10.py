fba497dddbd9eb50530a2c27d16e3407db920560
Payal Lakra <payallakra307@gmail.com>
2024-11-05 09:17:44 +0530

    Day10

--- Day10.py
+++ Day10.py
@@ -4,3 +4,14 @@ Topics to be Covered:
 - Functions with output
 - Docstrings
 '''
+
+#Convert into Title Case
+f_name = input("Enter your first name:")
+l_name = input("Enter your last name:")
+
+def name(f_name, l_name):
+    title_name = f"{f_name.title()} {l_name.title()}"
+    return title_name
+
+result = name(f_name=f_name, l_name=l_name)
+print(result)

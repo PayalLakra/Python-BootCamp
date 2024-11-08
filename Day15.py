0d69c6e9d52bf1bcd01a967fe42f1effd83e0ca0
Payal Lakra <payallakra307@gmail.com>
2024-11-08 23:03:15 +0530

    Day 15

---
+++ Day15.py
@@ -0,0 +1,10 @@
+'''
+Topics to be Covered:
+- Coffee machine Project
+'''
+
+# 3 Hot Flavours: Espresso(50ml W and 18g C - $1.50), Latte(200ml W 24g C and 150ml M - $2.50), Cappuccino(250ml W 24g C 100ml M - $3.00)
+#Resources : 300ml W , 200ml M , 100g C
+#Coin Operated: Penny - 1 cent , Nickel - 5 cents , Dime - 10 cents, Quarter - 25 cents
+
+# TO DO -> report(how much resources left), check resources sufficient , Porcess coins, check transaction successful, make coffee , deduct resources.  

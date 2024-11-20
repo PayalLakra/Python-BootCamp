9c42943ecf45f2223136b97eae519432c34f9136
Payal Lakra <payallakra307@gmail.com>
2024-11-20 23:45:59 +0530

    Initn Day12

--- Day12.py
+++ Day12.py
@@ -1,4 +1,7 @@
 '''
 Topics to be Covered:
--
+- Local and Global Scope
+- Number Guessing Game
 '''
+
+# Global constants are variables which you define & you're never planning on changing it ever again.

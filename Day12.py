6fed1a5d530ed22e62796f11f56bd6137991ae56
Payal Lakra <payallakra307@gmail.com>
2024-11-21 23:28:03 +0530

    Day12

--- Day12.py
+++ Day12.py
@@ -5,3 +5,51 @@ Topics to be Covered:
 '''
 
 # Global constants are variables which you define & you're never planning on changing it ever again.
+# Scope: -> Local Scope =  accessible only inside the function
+#        -> Global Scope =  accessible throughout the program
+
+name = "Outside Function"      # Global Scope
+
+def scope():
+    print("Inside Function")   # Local Scope
+
+print(name)   # Global Scope
+scope()       # Local Scope
+
+# Prime Number Checker
+
+num = int(input("Enter any number:"))
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2,num):
+        if num % i == 0:
+            return False
+    return True
+
+print(is_prime(num))
+
+# Number Guessing Game
+
+import random
+from art import number_guessing
+
+num = random.choice(1,100)
+print(num)
+
+def num_guess():
+    return
+
+print(number_guessing)
+print("Welcome to Number Guessing Game.")
+print("I'm thinking of a number between 1 and 100.")
+
+level = input("Choose a difficulty level. Type'easy' or 'hard'").lower()
+
+if level == 'easy':
+    print("You have 10 attempts remaining to guess the number.")
+    num_guess()
+elif level == 'hard':
+    print("You have 5 attempts remaining to guess the number.")
+    num_guess()

0ba81f3227455e83bd2eaac0908348ace940d63d
Payal Lakra <payallakra307@gmail.com>
2024-11-23 23:27:06 +0530

    Coffee machine using packages predefined

--- Day16.py
+++ Day16.py
@@ -40,14 +40,31 @@ table.add_column("Age", ["12","13","14"])
 table.align = "l"    #By default table is centered aligned and now we change it to left aligned.
 print(table)
 
+
+
 # COFEE MACHINE - Using OOP
 
 #Program Requirements - print report, check resources, process coins, check transaction successful, make coffee
 
-# from menu import Menu, MenuItem
-# from coffee_maker import CoffeeMaker
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
-money_machine.report()
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)

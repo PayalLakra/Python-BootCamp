f3159f5b3aafde3f8015f4c9a45f9660fc29135f
Payal Lakra <payallakra307@gmail.com>
2024-12-02 21:32:12 +0530

    Day27

---
+++ Day27/Day27.py
@@ -0,0 +1,9 @@
+'''
+Topics to be Covered:
+- Labels and Windows in tkinter
+- args, kwargs
+- Buttons, Entry and setting component options
+- Tkinter Widgets -  Radiobuttons, Scales, Checkbuttons
+- Tkinter layout Managers: pack(), place(), grid()
+- Project : Mile to kilometres Converter project
+'''

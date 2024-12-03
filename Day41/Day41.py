a9ad4c117482794a1a0b01f75044c81667e3e4bf
Payal Lakra <payallakra307@gmail.com>
2024-12-03 22:03:33 +0530

    Start Day41

---
+++ Day41/Day41.py
@@ -0,0 +1,13 @@
+'''Topics to be covered:
+- HTML
+- HTML Heading elements
+- HTML Paragraph Elements
+- Self Closing Tags
+- Project - Movie Ranking
+'''
+
+# HTML - responsible for the structure of website. It defines the content and structure of website.
+# CSS - responsible for the styling of website
+# JS - responsible for actually do things
+
+# HTML- Hyper Text Markup Language

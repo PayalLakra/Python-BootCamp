2c350e218e31f7479f942bae551ca97cf1e920ba
Payal Lakra <payallakra307@gmail.com>
2024-12-04 21:45:50 +0530

    Day41

--- Day41/Day41.py
+++ Day41/Day41.py
@@ -11,3 +11,9 @@
 # JS - responsible for actually do things
 
 # HTML- Hyper Text Markup Language
+
+# Heading Tag: <h1> </h1>. This is upto 6 only.
+# Heading element:  <h1>CONTENT</h1>
+# There should be only one h1. also don't skip the headings level
+
+# Paragraph Element: <p> </p>

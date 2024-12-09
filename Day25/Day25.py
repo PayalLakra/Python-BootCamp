99d4af6479830fb7288bdba42bc94f60b9775ef0
Payal Lakra <payallakra307@gmail.com>
2024-12-09 22:48:35 +0530

    Data read from csv

--- Day25/Day25.py
+++ Day25/Day25.py
@@ -4,3 +4,18 @@ Topics to be covered:
 - Data Frames
 - U.S. States game
 '''
+
+import csv
+
+with open("Day25\weather_data.csv","r") as data_file:
+    data = csv.reader(data_file)
+    temperature = []
+    for row in data:
+        print(row)
+
+#Pandas - to perform data analysis on tabular data
+
+import pandas
+
+data = pandas.read_csv("Day25\weather_data.csv")
+print(data)

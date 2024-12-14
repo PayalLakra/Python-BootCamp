85b04aead4c1b73b44ee48aee215ddb8fa8b0abe
Payal Lakra <payallakra307@gmail.com>
2024-12-14 22:50:08 +0530

    Day43

---
+++ Day43/Day43.py
@@ -0,0 +1,21 @@
+'''
+# Topics to be covered
+# - Ways to add CSS 
+# - CSS Selectors
+'''
+
+
+# 3 Ways to add CSS::
+# 1. Inline : <tag style = "css"/> ,  the inline CSS goes into the opening tag of the HTML.
+# -> Used when you only want to target a single element.
+# 2. Internal : <style>css</style>
+# -> Used when you only want to target a single web page.
+# 3. External : <link href="style.css"/> 
+# -> Used when you only want to target multi pages.
+
+# SELECTOR -> it's the part that selects the HTML in order to apply whichever rules go in between these curly braces.
+# Types of Selector
+# 1. Element Selector:: It selects a particular HTML tag.
+# 2. Class Selector::
+# 3. ID Selector::
+# 4. Attribute Selector::

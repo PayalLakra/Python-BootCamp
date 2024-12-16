c4e864b2f09da6ba8945c760b75b9a71e8c7a65c
Payal Lakra <payallakra307@gmail.com>
2024-12-16 19:52:51 +0530

    Labels & Windows in tkinter

--- Day27/Day27.py
+++ Day27/Day27.py
@@ -7,3 +7,14 @@ Topics to be Covered:
 - Tkinter layout Managers: pack(), place(), grid()
 - Project : Mile to kilometres Converter project
 '''
+
+import tkinter
+
+window = tkinter.Tk()                       # Window
+window.title("My first GUI Program.")       # Title
+window.minsize(width =100,height =300)      # Size
+
+my_label = tkinter.Label(text = "I am a Label", font =("Arial",24,"italic"))   # Label
+my_label.pack(side="left")          # To pack the label onto the screen
+
+window.mainloop()

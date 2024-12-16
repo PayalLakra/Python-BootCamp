035dac634802c8e4094c90a55f2b0751fc36ecef
Payal Lakra <payallakra307@gmail.com>
2024-12-16 21:14:13 +0530

    *args, **kwargs

--- Day27/Day27.py
+++ Day27/Day27.py
@@ -1,6 +1,7 @@
 '''
 Topics to be Covered:
 - Labels and Windows in tkinter
+- Advanced Python Argument
 - args, kwargs
 - Buttons, Entry and setting component options
 - Tkinter Widgets -  Radiobuttons, Scales, Checkbuttons
@@ -12,9 +13,36 @@ import tkinter
 
 window = tkinter.Tk()                       # Window
 window.title("My first GUI Program.")       # Title
-window.minsize(width =100,height =300)      # Size
+window.minsize(width =1000,height =300)      # Size
 
 my_label = tkinter.Label(text = "I am a Label", font =("Arial",24,"italic"))   # Label
 my_label.pack(side="left")          # To pack the label onto the screen
 
 window.mainloop()
+
+# Advanced Python Argument - Arguments with Default values
+def my_function(a=1,b=2,c=3):
+    print(a)
+    print(b)
+    print(c)
+
+my_function()
+my_function(b=5)     # Change only b value and rest remains same
+
+# Unlimited Arguments
+def adding(*args):
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
+
+print(adding(3,5,6,7,8,9,10))
+
+# **kwargs - Many keyworded arguments
+def calculate(n, **kwargs):
+    for key, value in kwargs.items():
+        n += kwargs["add"]
+        n *= kwargs["multiply"]
+        print(n)
+
+calculate(2, add=3, multiply=5)

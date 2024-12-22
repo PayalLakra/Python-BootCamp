fd1d186c05e237ef6ad64b041f7f04f0b144d7f8
Payal Lakra <payallakra307@gmail.com>
2024-12-22 21:17:24 +0530

    Send Emails with Python using SMTP - Working with datetime module

--- Day32/Day32.py
+++ Day32/Day32.py
@@ -6,3 +6,23 @@ Topics to be Covered:
 - Project- Automated Birthday Wisher Project
 - Run your Python code in the Cloud
 '''
+
+# SMTP: Simple Mail Transfer Protocol
+import smtplib
+
+my_email = "lakrapayal307@gmail.com"
+password = "PayalLakra@307"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="payallakra307@gmail.com",
+        msg="Subject:Hello\n\nThis is the SMTP."
+    )
+
+import datetime
+
+now = datetime.datetime.now()
+year = now.year

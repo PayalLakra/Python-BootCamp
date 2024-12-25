eec75e03c581b85f1a14f9ff15c580818f6b20a7
Payal Lakra <payallakra307@gmail.com>
2024-12-25 22:11:59 +0530

    API

---
+++ Day33/Day33.py
@@ -0,0 +1,12 @@
+'''
+Topics to be covered:
+- API
+- API Endpoint and making calls
+- Working with responses-
+- Challenge
+- UNderstand API Parameters
+- ISS Overhead Notifier Project
+'''
+
+# API's are  all are a set of commands, functions, protocols, and objects that programmers can use to create software or interact with an external system.
+# API is an interface or rather a sort of barrier between your program and an external system.

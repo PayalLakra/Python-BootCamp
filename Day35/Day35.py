f99b05aea7c299883583db2638debc6c84a97dee
Payal Lakra <payallakra307@gmail.com>
2024-12-28 12:05:52 +0530

    Start Day35

---
+++ Day35/Day35.py
@@ -0,0 +1,9 @@
+'''
+Topics to be Covered:
+- API Authentication and its need
+- Use of API Keys to Authenticate and get Weather from OpenWeatherMap
+- Challenge - Check if it will rain in next 12 Hours
+- Sending SMS via the Twilio API
+- Use PythonAnywhere to Automate Python Script
+- Understanding Environment Variables and Hiding API keys
+'''

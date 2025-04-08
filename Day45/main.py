b68a958059631741af08f3ea34b382315623e5f4
Payal Lakra <payallakra307@gmail.com>
2025-04-08 21:38:22 +0530

    BeautifulSoup Anchor Tags

--- Day45/main.py
+++ Day45/main.py
@@ -12,3 +12,17 @@ print(soup.find_all(name="a"))  # This will print all the anchor tags in the HTM
 print(soup.find_all(class_="heading"))  # This will print all the elements with class "heading" in the HTML document.
 print(soup.select("h1"))  # This will print all the h1 tags in the HTML document.
 print(soup.select(".heading"))  # This will print all the elements with class "heading" in the HTML document.
+
+all_anchor_tags = soup.find_all(name="a")  # This will find all anchor tags in the HTML document.
+
+for tag in all_anchor_tags:
+    print(tag.get("href"))  # This will print the href attribute of each anchor tag.
+
+heading = soup.find(name="h1", id="name")  # This will find the h1 tag with id "name" in the HTML document.
+print(heading.text)  # This will print the text of the heading.
+
+name = soup.select_one("#name")  # This will select the element with id "name" in the HTML document.
+print(name)  # This will print the element with id "name" in the HTML document.
+
+soup.select_one(".heading")  # This will select the first element with class "heading" in the HTML document.
+print(heading)

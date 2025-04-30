de88acfccfec7ee92e6dde990af1d4712595fd2d
Payal Lakra <payallakra307@gmail.com>
2025-04-30 08:57:54 +0530

    Combining Jinja template with APIs

---
+++ Day57/Day57.py
@@ -0,0 +1,7 @@
+'''Topics to be covered-
+- URL Building and Templating with Jinja in your Flask Application
+- Works with API
+'''
+
+# Jinja is a templating engine for Python that allows you to create dynamic HTML pages by embedding Python code within HTML templates.
+# Flask uses Jinja as its default templating engine, which makes it easy to generate HTML pages with dynamic content.
---
+++ Day57/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/guess/<name>")
+def guess(name):
+    gender_url = f"https://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+
+    age_url = f"https://api.agify.io?name={name}"
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+    return render_template('guess.html', name=name, gender=gender, age=age)
+
+if __name__ == '__main__':
+    app.run(debug=True)

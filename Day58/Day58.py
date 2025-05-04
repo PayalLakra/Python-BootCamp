c1619cec22f77e6d4046bc81862e24765a5ab53d
Payal Lakra <payallakra307@gmail.com>
2025-05-04 22:27:00 +0530

    Bootstrap

--- Day58/Day58.py
+++ Day58/Day58.py
@@ -5,3 +5,5 @@ Topics to be Covered:
 - Bootstrap Components
 - Project- A Startup Website for TinDog
 '''
+
+# Bootstrap is a CSS framework that helps you create responsive and mobile-first websites quickly and easily. It provides a set of pre-designed components and a grid system that makes it easy to create layouts that work on different screen sizes. 

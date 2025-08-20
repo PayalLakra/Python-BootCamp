0ecc74802da3fd97676780edea57e9d5a9078cef
Payal Lakra <payallakra307@gmail.com>
2025-08-20 14:48:45 +0530

    Data Exploration with Pandas

---
+++ Day72/Day72.py
@@ -0,0 +1,31 @@
+'''
+Topics to be covered:
+- Data Exploration with Pandas
+'''
+
+'''
+Today we'll learn:
+- How to explore a Pandas DataFrame
+- How to detect NaN (not a number) values and clean your data
+- How to select particular columns, rows, and individual cells
+- How to sort your data
+- How to group data by category
+and so much more!
+'''
+
+# Today's Learning Points ->
+
+
+# Use .head(), .tail(), .shape and .columns to explore your DataFrame and find out the number of rows and columns as well as the column names.
+
+# Look for NaN (not a number) values with .findna() and consider using .dropna() to clean up your DataFrame.
+
+# You can access entire columns of a DataFrame using the square bracket notation: df['column name'] or df[['column name 1', 'column name 2', 'column name 3']]
+
+# You can access individual cells in a DataFrame by chaining square brackets df['column name'][index] or using df['column name'].loc[index]
+
+# The largest and smallest values, as well as their positions, can be found with methods like .max(), .min(), .idxmax() and .idxmin()
+
+# You can sort the DataFrame with .sort_values() and add new columns with .insert()
+
+# To create an Excel Style Pivot Table by grouping entries that belong to a particular category use the .groupby() method
